028dc785948763b8b4b170a079984a08345131bd
opnegion <opnegion>
2017-09-29 14:35:54 +0900

    update

--- lib/java_buildpack/container/tomcat.rb
+++ lib/java_buildpack/container/tomcat.rb
@@ -49,7 +49,7 @@ module JavaBuildpack
           TomcatInstance.new(sub_configuration_context(context, 'tomcat')),
           TomcatLifecycleSupport.new(sub_configuration_context(context, 'lifecycle_support')),
           TomcatLoggingSupport.new(sub_configuration_context(context, 'logging_support')),
-          TomcatAccessLoggingSupport.new(sub_configuration_context(context, 'access_logging_support')),
+          
           TomcatRedisStore.new(sub_configuration_context(context, 'redis_store')),
           TomcatInsightSupport.new(context)
         ]
